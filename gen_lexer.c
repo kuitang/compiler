109bd8e1bfd8f30433818b61a02b5d1a8eb07a47
Kui Tang <kuitang@gmail.com>
2023-09-13 23:55:36 -0400

    [SUCCESS] puncts success

--- gen_lexer.c
+++ gen_lexer.c
@@ -22,19 +22,6 @@ static int PUNCT_VALUES_len[N_PUNCTS];
 static int max_KEYWORD_VALUES_len = -1;
 static int KEYWORD_VALUES_len[N_KEYWORDS];
 
-static char is_punct_token[256];
-static char punct_successor[256];
-
-/*
-typedef int (*compar_t)(const void *, const void *);
-
-int indirect_strcmp(const void **p1, const void **p2) {
-  const char *s1 = *p1;
-  const char *s2 = *p2;
-  return strcmp(s1, s2);
-}
-*/
-
 typedef struct {
   // static
   const char *buf;
@@ -59,10 +46,8 @@ char peek(ScannerCont *cont) {
 
 char getch(ScannerCont *cont) {
   THROW_IF(cont->pos == cont->size, EXC_INTERNAL, "EOF reached");
-  // DEBUG_PRINT_EXPR("before %d: %c", cont->pos, cont->buf[cont->pos]);
   char ret = cont->buf[cont->pos];
   cont->pos++;
-  // DEBUG_PRINT_EXPR("after %d: %c", cont->pos, cont->buf[cont->pos]);
   return ret;
   // return cont->buf[cont->pos++];
 }
@@ -101,6 +86,7 @@ int match_keyword_or_ident(ScannerCont *cont) {
         ungetch(cont);
         return i;
       }
+      ungetch(cont);
       return -1;
     }
     // advance i such that strings[i][k] == ch
@@ -118,43 +104,85 @@ int match_keyword_or_ident(ScannerCont *cont) {
     }
     // DEBUG_PRINT("Next getch...");
   }
+  ungetch(cont);
   return -1;
 }
 
-Token consume_next_token(ScannerCont *cont) {
-  while (isspace(getch(cont)))
-    ;
+// essentially a copy of above, but different exit logic
+int match_punct(ScannerCont *cont) {
+  DEBUG_PRINT_EXPR("new call %d", cont->pos);
+  int i = 0;
+  int i_prev_match = -1;
+  int matched = 1;
+  char ch;
+  int k;
+  for (k = 0; matched; k++) {
+    ch = getch(cont);
+    // advance i such that strings[i][k] == ch
+    // DEBUG_PRINT_EXPR("%d %d", i, n_keywords);
+    for(matched = 0; i < N_PUNCTS; i++) {
+      if (PUNCT_VALUES_len[i] < k + 1) {
+        continue;
+      }
+      if (PUNCT_VALUES[i][k] == ch) {
+        DEBUG_PRINT("**************** GOT HERE ******************");
+        DEBUG_PRINT_EXPR("%d %c %d %c %s", k, ch, i, PUNCT_VALUES[i][k], PUNCT_VALUES[i]);
+        i_prev_match = i;
+        matched = 1;
+        break;
+      }
+    }
+    // Eventually k will be large enough that we don't match, exiting the for loop.
+  }
+  assert(!matched);
+  if (i_prev_match >= 0) {
+    // ch didn't match, but the previous k matched. Return current character and return previous match.
+    ungetch(cont);
+    DEBUG_PRINT_EXPR("Returning... %d %c %d %c %s", k, ch, i, PUNCT_VALUES[i_prev_match][k], PUNCT_VALUES[i_prev_match]);
+    return i_prev_match;
+  }
   ungetch(cont);
-  int ch = peek(cont);
+  return -1;
+}
+
+// Anytime we return to consume_next_token, we are not in the "middle" of any token. So any helper function that reads
+// one character too many must ungetch it before returning.
+Token consume_next_token(ScannerCont *cont) {
+  char ch;
+  char *msg;
+label_start:
+  ch = peek(cont);
   if (ch == '\0') {
     return (Token) {.kind = TOK_END_OF_FILE, .value = 0, .len = 0};
   }
-  if (is_punct_token[ch]) {
-    char *buf = malloc(3);
-    buf[0] = ch;
-    getch(cont);
-    int next_ch = peek(cont);
-    if (next_ch == punct_successor[ch]) {
-      getch(cont);
-      // fuck memory
-      buf[1] = next_ch;
-      buf[2] = '\0';
-      return (Token) {.kind = TOK_ELLIPSIS, .value = buf, .len = 1};
+  if (isspace(ch)) {
+    while (isspace(getch(cont)))
+      ;
+    ungetch(cont);
+    assert(!isspace(peek(cont)));
+    goto label_start;
+  }
+  if (ispunct(ch)) {
+    int start_pos = cont->pos;
+    int i = match_punct(cont);
+    if (i >= 0) {
+      return (Token) {.kind = PUNCT_KIND(i), .value = PUNCT_VALUES[i], .len = PUNCT_VALUES_len[i]};
     }
-    buf[1] = '\0';
-    return (Token) {.kind = TOK_ELLIPSIS, .value = buf, .len = 2};
+    // if we didn't consume anything at all, then ch is punct but doesn't match and valid token. 
+    if (cont->pos == start_pos)
+      goto label_error;
+    // next char could be anything
+    goto label_start;
   }
   if (is_ident_start(ch)) {
     // identifier_or_keyword
     int start_pos = cont->pos;
-    int lc_ret = match_keyword_or_ident(cont);
-    if (lc_ret >= 0) {
+    int i = match_keyword_or_ident(cont);
+    if (i >= 0) {
       // found keyword
-      return (Token) {.kind = KEYWORD_KIND(lc_ret), .value = KEYWORD_VALUES[lc_ret], .len = KEYWORD_VALUES_len[lc_ret]};
+      return (Token) {.kind = KEYWORD_KIND(i), .value = KEYWORD_VALUES[i], .len = KEYWORD_VALUES_len[i]};
     }
-    // else we are in the middle of a word
-    ungetch(cont);
-    // we are in the middle of a word
+    // else we are in the middle of an identifier
     while (is_ident_rest(getch(cont)))
       ;
     ungetch(cont);
@@ -169,10 +197,11 @@ Token consume_next_token(ScannerCont *cont) {
     int span_len = cont->pos - start_pos;
     return (Token) {.kind = TOK_INTEGER_LITERAL, .value = cont->buf + start_pos, .len = span_len};
   }
-  char *msg = malloc(1000);
+label_error:
+  // Syntax error if we get here
+  msg = malloc(1000);
   snprintf(msg, 1000, "Invalid character %c at position %d", ch, cont->pos);
   THROW(EXC_LEX_SYNTAX, msg);
-  assert(0 && "Unreachable state!");
 }
 
 void parse_start(FILE *in) {
@@ -200,32 +229,19 @@ void parse_start(FILE *in) {
   }
 }
 
-void fill_punct_tables() {
-  // tokens must be lexicographically sorted
-  int c1;
-  for (int i = 0; i < N_PUNCTS; i++) {
-    c1 = PUNCT_VALUES[i][0];
-    is_punct_token[c1] = 1;
-    if (PUNCT_VALUES_len[i] == 2) {
-      punct_successor[c1] = PUNCT_VALUES[i][1];
-    }
-  }
-}
-
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stderr, "gen_lexer: no input file\n");
     exit(1);
   }
-  fprintf(stderr, "punctations:\n");
+  printf("punctations:\n");
   for (int i = 0; i < N_PUNCTS; i++) {
-    fprintf(stderr, "%8d: %s\n", i, PUNCT_VALUES[i]);
+    printf("%8d: %s\n", i, PUNCT_VALUES[i]);
   }
-  fprintf(stderr, "keywords:\n");
+  printf("keywords:\n");
   for (int i = 0; i < N_KEYWORDS; i++) {
-    fprintf(stderr, "%8d: %s\n", i, KEYWORD_VALUES[i]);
+    printf("%8d: %s\n", i, KEYWORD_VALUES[i]);
   }
-  fill_punct_tables();
   #define n_PUNCT_VALUES N_PUNCTS
   fill_lens(PUNCT_VALUES);
   // hack
